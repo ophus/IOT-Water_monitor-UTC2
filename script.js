b419f1a67edf2bacfb3836bbf1054cd5af3763c8
ophus <149864924+ophus@users.noreply.github.com>
2025-03-14 19:51:49 +0700

    Update script.js
    change fetch to https://esp32-data-receiver.phucminh9395.workers.dev/

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 async function getData() {
-    const response = await fetch("https://esp32-data-receiver.phucminh9395.workers.dev/");
+    const response = await fetch("https://esp32-data-receiver.phucminh9395.workers.dev/", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        mode: "cors", // Quan trọng để tránh lỗi CORS
+    });
+
     const data = await response.json();
 
     document.getElementById("tempValue").innerText = `${data.temperature} °C`;
@@ -7,5 +14,4 @@ async function getData() {
     document.getElementById("TDSValue").innerText = `${data.tds} PPM`;
 }
 
-setInterval(getData, 5000);  
-
+setInterval(getData, 5000);
